66b6c2119b335b8d561002bb770d997fd952adf1
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2020-10-29 13:35:02 -0400

    api

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 const server = require("./api/server.js");
 
-const port = process.env.PORT || 5000;
-const secret = process.env.SECRET_THING || 'the secret for development'
+const port = process.env.PORT || 4000;
+const secret = process.env.SECRET_THING || 'foo';
 
-console.log(port, secret)
+console.log(port, secret);
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
